16810b514af4e81b8dfd06f92c3730c6f632bf81
singular.seal <singular.seal@gmail.com>
2022-10-30 17:24:26 +0800

    temp commit

--- pkg/processor/mapping/dbchange.go
+++ pkg/processor/mapping/dbchange.go
@@ -14,7 +14,7 @@ type DBInfo struct {
 type TableInfo struct {
 	targetTable   string
 	actionMapping map[string]string
-	columnMapping map[string]*string
+	columnMapping map[string]string
 }
 
 type DBChangeMappingProcessor struct {
@@ -58,86 +58,101 @@ func (p *DBChangeMappingProcessor) Configure(config core.StringMap) error {
 			targetDB: targetDB,
 		}
 		p.mappings[sourceDB] = dbInfo
-		// parse table mapping
-		sourceTables, err := utils.GetConfigArrayFromConfig(source, "$.Tables")
-		if err != nil || len(sourceTables) == 0 {
-			continue
-		}
-		targetTables, err := utils.GetConfigArrayFromConfig(target, "$.Tables")
+
+		configureTableMapping(dbInfo, source, target)
+	}
+	return nil
+}
+
+func configureTableMapping(dbInfo *DBInfo, source core.StringMap, target core.StringMap) error {
+	sourceTables, err := utils.GetConfigArrayFromConfig(source, "$.Tables")
+	if err != nil || len(sourceTables) == 0 {
+		return nil
+	}
+	targetTables, err := utils.GetConfigArrayFromConfig(target, "$.Tables")
+	if err != nil {
+		return err
+	}
+	if len(sourceTables) != len(targetTables) {
+		return fmt.Errorf("source table count and target table count not equal")
+	}
+	dbInfo.tableMapping = map[string]*TableInfo{}
+	for i, sourceTableMap := range sourceTables {
+		targetTableMap := targetTables[i]
+		sourceTable, err := utils.GetStringFromConfig(sourceTableMap, "$.Table")
 		if err != nil {
 			return err
 		}
-		if len(sourceTables) != len(targetTables) {
-			return fmt.Errorf("source table count and target table count not equal")
+		targetTable, err := utils.GetStringFromConfig(targetTableMap, "$.Table")
+		if err != nil {
+			return err
 		}
-		dbInfo.tableMapping = map[string]*TableInfo{}
-		for i, sourceTableMap := range sourceTables {
-			targetTableMap := targetTables[i]
-			sourceTable, err := utils.GetStringFromConfig(sourceTableMap, "$.Table")
-			if err != nil {
-				return err
-			}
-			targetTable, err := utils.GetStringFromConfig(targetTableMap, "$.Table")
-			if err != nil {
-				return err
-			}
-			tableInfo := &TableInfo{
-				targetTable: targetTable,
-			}
-			dbInfo.tableMapping[sourceTable] = tableInfo
-			// parse column mapping
-			sourceColumns, err := utils.GetConfigArrayFromConfig(sourceTableMap, "$.Columns")
-			if err != nil || len(sourceColumns) == 0 {
-				continue
-			}
-			targetColumns, err := utils.GetConfigArrayFromConfig(targetTableMap, "$.Columns")
-			if err != nil {
-				return err
-			}
-			if len(sourceColumns) != len(targetColumns) {
-				return fmt.Errorf("source column count and target column count not equal")
-			}
-			tableInfo.columnMapping = map[string]*ColumnInfo{}
-			for j, sourceColumnMap := range sourceColumns {
-				targetColumnMap := targetColumns[j]
-				sourceColumn, err := utils.GetStringFromConfig(sourceColumnMap, "$.Column")
-				if err != nil {
-					return err
-				}
-				targetColumn, err := utils.GetStringFromConfig(targetColumnMap, "$.Column")
-				if err != nil {
-					return err
-				}
-				columnInfo := &ColumnInfo{
-					targetColumn: targetColumn,
-				}
-				tableInfo.columnMapping[sourceColumn] = columnInfo
-				// parse action mapping
-				sourceActions, err := utils.GetArrayFromConfig(sourceColumnMap, "$.Actions")
-				if err != nil || len(sourceActions) == 0 {
-					continue
-				}
-				targetActions, err := utils.GetArrayFromConfig(targetColumnMap, "$.Actions")
-				if err != nil {
-					return err
-				}
-				if len(sourceActions) != len(targetActions) {
-					return fmt.Errorf("source action count and target action count not equal")
-				}
-				columnInfo.actionMapping = map[string]string{}
-				for k, each := range sourceActions {
-					sourceAction, ok := each.(string)
-					if ok {
-						return fmt.Errorf("source action is not string")
-					}
-					targetAction, ok := targetActions[k].(string)
-					if ok {
-						return fmt.Errorf("target action is not string")
-					}
-					columnInfo.actionMapping[sourceAction] = targetAction
-				}
-			}
+		tableInfo := &TableInfo{
+			targetTable: targetTable,
 		}
+		dbInfo.tableMapping[sourceTable] = tableInfo
+
+		if err = configureColumnMapping(tableInfo, sourceTableMap, targetTableMap); err != nil {
+			return err
+		}
+		if err = configureActionMapping(tableInfo, sourceTableMap, targetTableMap); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func configureActionMapping(tableInfo *TableInfo, sourceTableMap core.StringMap, targetTableMap core.StringMap) error {
+	sourceActions, err := utils.GetArrayFromConfig(sourceTableMap, "$.Actions")
+	if err != nil || len(sourceActions) == 0 {
+		return nil
+	}
+	targetActions, err := utils.GetArrayFromConfig(targetTableMap, "$.Actions")
+	if err != nil {
+		return err
+	}
+	if len(sourceActions) != len(targetActions) {
+		return fmt.Errorf("source action count and target action count not equal")
+	}
+	tableInfo.actionMapping = map[string]string{}
+	for k, each := range sourceActions {
+		sourceAction, ok := each.(string)
+		if ok {
+			return fmt.Errorf("source action is not string")
+		}
+		targetAction, ok := targetActions[k].(string)
+		if ok {
+			return fmt.Errorf("target action is not string")
+		}
+		tableInfo.actionMapping[sourceAction] = targetAction
+	}
+	return nil
+}
+
+func configureColumnMapping(tableInfo *TableInfo, sourceTableMap core.StringMap, targetTableMap core.StringMap) error {
+	sourceColumns, err := utils.GetArrayFromConfig(sourceTableMap, "$.Columns")
+	if err != nil || len(sourceColumns) == 0 {
+		return nil
+	}
+	targetColumns, err := utils.GetArrayFromConfig(targetTableMap, "$.Columns")
+	if err != nil {
+		return err
+	}
+	if len(sourceColumns) != len(targetColumns) {
+		return fmt.Errorf("source column count and target column count not equal")
+	}
+	tableInfo.columnMapping = map[string]string{}
+	for j, sourceColumnObj := range sourceColumns {
+		targetColumnObj := targetColumns[j]
+		sourceColumn, ok := sourceColumnObj.(string)
+		if !ok {
+			return fmt.Errorf("source column is not string")
+		}
+		targetColumn, ok := targetColumnObj.(string)
+		if !ok {
+			return fmt.Errorf("target column is not string")
+		}
+		tableInfo.columnMapping[sourceColumn] = targetColumn
 	}
 	return nil
 }
@@ -157,9 +172,63 @@ func (p *DBChangeMappingProcessor) Process(msg *core.Message) (skip bool, err er
 		return
 	}
 	event.Table = tableInfo.targetTable
-	if tableInfo.columnMapping == nil {
-		return
+
+	if tableInfo.actionMapping != nil {
+		processActionMapping(event, tableInfo.actionMapping)
 	}
-	event.GetRow()
+	if tableInfo.columnMapping != nil {
+		processColumnMapping(event, tableInfo.columnMapping)
+	}
+
 	return
+}
+
+func processColumnMapping(event *core.DBChangeEvent, columnMapping map[string]string) {
+	rows := make([]map[string]interface{}, 0)
+	if event.NewRow != nil {
+		rows = append(rows, event.NewRow)
+	}
+	if event.OldRow != nil {
+		rows = append(rows, event.OldRow)
+	}
+	for _, row := range rows {
+		for k, v := range row {
+			if col, ok := columnMapping[k]; ok && col != k {
+				row[col] = v
+				delete(row, k)
+			}
+		}
+	}
+}
+
+func processActionMapping(event *core.DBChangeEvent, actionMapping map[string]string) {
+	if op, ok := actionMapping[event.Operation]; ok {
+		switch event.Operation {
+		case core.DBInsert:
+			switch op {
+			case core.DBUpdate:
+				event.OldRow = event.NewRow
+			case core.DBDelete:
+				event.OldRow = event.NewRow
+				event.NewRow = nil
+			}
+		case core.DBUpdate:
+			switch op {
+			case core.DBInsert:
+				event.OldRow = nil
+			case core.DBDelete:
+				event.OldRow = event.NewRow
+				event.NewRow = nil
+			}
+		case core.DBDelete:
+			switch op {
+			case core.DBInsert:
+				event.NewRow = event.OldRow
+				event.OldRow = nil
+			case core.DBUpdate:
+				event.NewRow = event.OldRow
+			}
+		}
+		event.Operation = op
+	}
 }
