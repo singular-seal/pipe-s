889996ca45c654c05f76286c2b06399b7eaaea32
singular.seal <singular.seal@gmail.com>
2023-01-24 19:48:38 +0800

    commit

--- integration_test/dbsync/data_integrity_test_linux.sh
+++ integration_test/dbsync/data_integrity_test_linux.sh
@@ -54,20 +54,40 @@ function init_target_db() {
   mysql -h$TARGET_HOST -P$TARGET_PORT -u$TARGET_USER -p$TARGET_PASSWORD $DATABASE <$TABLE_STRUCTURE_FILE
 }
 
-function wait_for_end() {
+function wait_for_sync_finished() {
   sleep 10
   while true; do
     is_syncing
     if [ $? -ne 1 ]; then
       return
     fi
-    echo "syncing data ..."
+    echo "waiting for sync finish"
     sleep 10
   done
 }
 
 function kill_sync_process() {
   ps -ef | grep "$WORK_DIR/$DB_SYNC_CONFIG" | grep -v grep | awk '{print $2}' | xargs kill -9
+}
+
+function wait_for_sync_process_stopped() {
+  while true; do
+    count=$(lsof -i:$METRICS_PORT | wc -l)
+    if [ "$count" -eq 0 ]; then
+      echo "syncing stopped ..."
+      return
+    fi
+    sleep 1
+  done
+}
+
+function randomly_restart_sync() {
+  delay=$((RANDOM % 40 + 10))
+  echo "restart after $delay seconds"
+  sleep $((delay))
+  kill_sync_process
+  wait_for_sync_process_stopped
+  nohup $WORK_DIR/$BINARY --config $WORK_DIR/$DB_SYNC_CONFIG &
 }
 
 while getopts 'i' OPT; do
@@ -79,12 +99,13 @@ done
 init_target_db
 cp "${DB_SYNC_STATE_FILE}.bak" $DB_SYNC_STATE_FILE
 nohup $WORK_DIR/$BINARY --config $WORK_DIR/$DB_SYNC_CONFIG &
-wait_for_end
+randomly_restart_sync
+wait_for_sync_finished
 kill_sync_process
 
 echo "begin checking db"
-rm "$DB_DB_CHECK_STATE_FILE"
+rm "$DB_CHECK_STATE_FILE"
+rm "$DB_CHECK_RESULT_FILE"
 $WORK_DIR/$BINARY --config $WORK_DIR/$DB_CHECK_CONFIG
-
 
 echo "all done"
