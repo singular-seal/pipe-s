01a71df52254b32c860389fcd39f3974bc4b5748
singular.seal <singular.seal@gmail.com>
2023-03-06 20:45:12 +0800

    commit

--- pkg/output/debug/output.go
+++ pkg/output/debug/output.go
@@ -18,7 +18,7 @@ func NewDebugOutput() *DebugOutput {
 func (out *DebugOutput) Process(msg *core.Message) {
 	event := msg.Data.(*core.DBChangeEvent)
 	id := event.GetRow()["id"]
-	if id.(int32) == 2963 {
+	if id.(int32) == 13908 {
 		fmt.Println(fmt.Sprintf("id:%s op:%s", msg.Header.ID, event.Operation))
 	}
 	out.GetInput().Ack(msg, nil)
