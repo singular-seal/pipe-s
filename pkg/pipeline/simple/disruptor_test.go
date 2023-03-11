2df26bb384a8b9ba06817b02daa52133617685d9
singular.seal <singular.seal@gmail.com>
2023-03-12 06:52:43 +0800

    Add a test case for ack order guarantee.

--- pkg/pipeline/simple/disruptor_test.go
+++ pkg/pipeline/simple/disruptor_test.go
@@ -4,23 +4,32 @@ import (
 	"fmt"
 	"github.com/singular-seal/pipe-s/pkg/core"
 	"github.com/stretchr/testify/require"
-	"sync/atomic"
+	"strconv"
 	"testing"
 	"time"
 )
 
 type TestOutput struct {
 	*core.BaseOutput
-	Count int64
+	Count    int
+	Current  int
+	Disorder bool
 }
 
 func (out *TestOutput) Process(msg *core.Message) {
-	atomic.AddInt64(&out.Count, 1)
+	out.Count++
+	id, _ := strconv.Atoi(msg.Header.ID)
+	if id < out.Current {
+		out.Disorder = true
+	}
+	out.Current = id
 	out.GetInput().Ack(msg, nil)
 }
 
 type TestInput struct {
 	*core.BaseInput
+	Current  int
+	Disorder bool
 }
 
 func (in *TestInput) SetState(state []byte) error {
@@ -32,11 +41,16 @@ func (in *TestInput) GetState() (state []byte, done bool) {
 }
 
 func (in *TestInput) Ack(msg *core.Message, err error) {
+	id, _ := strconv.Atoi(msg.Header.ID)
+	if id < in.Current {
+		in.Disorder = true
+	}
+	in.Current = id
 }
 
 func (in *TestInput) Start() (err error) {
 	ms := make([]*core.Message, 0)
-	for i := 0; i < 10000; i++ {
+	for i := 0; i < 1000000; i++ {
 		m := &core.Message{
 			Header: &core.MessageHeader{
 				ID:      fmt.Sprintf("%d", i),
@@ -59,12 +73,14 @@ func TestDisruptorPipeline_Process(t *testing.T) {
 	})
 	in := &TestInput{
 		BaseInput: core.NewBaseInput(),
+		Current:   -1,
 	}
 	p.SetInput(in)
 	in.SetOutput(p)
 
 	o := &TestOutput{
 		BaseOutput: core.NewBaseOutput(),
+		Current:    -1,
 	}
 	p.SetOutput(o)
 	o.SetInput(p)
@@ -72,5 +88,7 @@ func TestDisruptorPipeline_Process(t *testing.T) {
 	p.Start()
 
 	time.Sleep(time.Second)
-	r.Equal(o.Count, int64(10000))
+	r.False(in.Disorder)
+	r.False(o.Disorder)
+	r.Equal(o.Count, 1000000)
 }
